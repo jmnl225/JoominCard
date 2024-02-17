7bd6d3fa9ab5d8d22405a59667c9fe3aba2a30d6
joomin <jmnl225@github.com>
2024-02-17 22:31:20 +0900

    Initial Commit

---
+++ JoominCard/JoominCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoominCardApp.swift
+//  JoominCard
+//
+//  Created by joomin on 2024/02/17.
+//
+
+import SwiftUI
+
+@main
+struct JoominCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

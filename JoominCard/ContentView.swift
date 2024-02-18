dd472b8391aa1b67def35ee927e660bd169d1868
joomin <jmnl225@github.com>
2024-02-18 11:08:34 +0900

    joomin card

--- JoominCard/ContentView.swift
+++ JoominCard/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("joomin")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                
+                Text("Joomin Lee")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                
+                InfoView(text: "+82 123 456 789", imageName: "phone.fill")
+                
+                InfoView(text: "joominl15@gmail.com", imageName: "envelope.fill")
+            }
         }
-        .padding()
     }
 }
 
@@ -24,3 +42,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ JoominCard/InfoView.swift
@@ -0,0 +1,34 @@
+//
+//  InfoView.swift
+//  JoominCard
+//
+//  Created by joomin on 2024/02/18.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(
+                HStack {
+                    Image(systemName: imageName)
+                        .foregroundColor(.green)
+                    Text(text).foregroundColor(.black)
+                }
+            )
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
